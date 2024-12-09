9cd8c614069ad315159308dd8d365328c742c2f2
Ruslan <RuslanKirpichnikov@gmail.com>
2024-12-09 19:53:04 +0300

    Refactor

--- src/blender_service/router.py
+++ src/blender_service/router.py
@@ -4,6 +4,7 @@ from fastapi import (
     APIRouter,
     UploadFile,
     Depends,
+    BackgroundTasks,
     status,
 )
 from fastapi.responses import StreamingResponse
@@ -24,9 +25,11 @@ from .schemas import (
     JobManager,
     RenderResult,
 )
+from .service import render_job
 from .dependencies import get_job_or_404
 from .constants import JobErrorMessages
-from .tasks import render_job_task
+
+# from .tasks import render_job_task
 
 
 router = APIRouter(prefix="/renders")
@@ -58,6 +61,7 @@ async def upload_file(
 def start_render(
     job_id: str,
     render_settings: RenderSettings,
+    background_tasks: BackgroundTasks,
     redis: Redis = Depends(get_jobs_redis),
 ):
     inspector = celery.control.inspect()
@@ -76,11 +80,12 @@ def start_render(
     if job.status == Status.RENDERING:
         raise BadRequestError(JobErrorMessages.JOB_ALREADY_RENDERING.value)
 
-    task = render_job_task.delay(job_id)
+    # task = render_job_task.delay(job_id)
+    background_tasks.add_task(render_job, job_id)
 
     job.render_settings = render_settings
     job.status = Status.RENDERING
-    job.task_id = task.id
+    # job.task_id = task.id
 
     JobManager.save(job, redis)
 
